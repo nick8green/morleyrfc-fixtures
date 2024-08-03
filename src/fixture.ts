54af53e283c04cd6b59df5470bac81ded2f8e353
Nick Green <nick-green@live.co.uk>
2024-08-03 22:08:50 +0100

    enhanced for the new season

--- src/fixture.ts
+++ src/fixture.ts
@@ -1,2 +1,84 @@
-// import { Club } from "team"
+import { Club, findTeam, Team } from "./team"
 
+export interface Match {
+    date: Date;
+    meet: Date;
+    opposition: Club;
+    team: Team;
+    type: MatchType;
+    venue: Venue;
+}
+
+export enum MatchType {
+    YORKSHIRE = 'Yorkshire Cup',
+    NATIONAL = 'Papa John\'s National Cup',
+    FIRST_TEAM = 'Regional 2 North East',
+    SECOND_TEAM = 'Yorkshire Merit League Championship',
+    FRIENDLY = 'Friendly',
+}
+
+export enum Venue {
+    AWAY = 'Away match',
+    HOME = 'Home match',
+}
+
+export const getFixture = (team: Team, row: any, opposition: string) => {
+    const oppo = findTeam(opposition);
+    const [type, venue] = getDetails(team, row);
+    const [date, meet] = getTimings(row[0])
+    return {
+        date,
+        meet,
+        opposition: oppo,
+        team,
+        type: getType(oppo, type),
+        venue,
+    };
+};
+
+const getDetails = (team: Team, row: any[]): [MatchType, Venue] => {
+    const index = team === Team.MAROONS ? 2 : 4;
+
+    let comp = MatchType.FRIENDLY;
+    let venue = Venue.HOME;
+
+    if (/\*$/.exec(row[index])) {
+        comp = team === Team.MAROONS ? MatchType.FIRST_TEAM : MatchType.SECOND_TEAM;
+    }
+
+    if (/^[Aa]/.exec(row[index])) {
+        venue = Venue.AWAY;
+    }
+
+    return [comp, venue];
+};
+
+const getTimings = (time: number): [Date, Date] => {
+    const date = excelDateToJSDate(time);
+    date.setHours(15);
+
+    if ([0, 1, 10, 11].includes(date.getMonth())) {
+        date.setHours(14, 15);
+    }
+
+    const meet = new Date(date.getTime());;
+    meet.setHours(date.getHours() - 2)
+
+    return [date, meet];
+};
+
+const excelDateToJSDate = (date: number): Date => {
+    return new Date(Math.round((date - 25569) * 86400 * 1000));
+};
+
+const getType = (opposition: Club, type: MatchType): MatchType => {
+    switch (opposition.name) {
+        case 'Yorkshire Cup Reserve Date':
+        case 'YC ?':
+            return MatchType.YORKSHIRE;
+        case 'National Cup Reserve Date':
+            return MatchType.NATIONAL;
+        default:
+            return type;
+    }
+};
--- src/team.ts
+++ src/team.ts
@@ -3,31 +3,66 @@ export interface Club {
     name: string
 }
 
-export const opposition: Club[] = [
-    { ground: '', name: 'Harrogate Pythons' },
-    { ground: '', name: 'Fire Service' },
-    { ground: '', name: 'Heath' },
-    { ground: '', name: 'Old Crossleyans' },
-    { ground: '', name: 'Goole' },
-    { ground: '', name: 'Rochdale' },
-    { ground: '', name: 'Scarborough' },
-    { ground: '', name: 'Selby' },
-    { ground: '', name: 'Glossop' },
-    { ground: '', name: 'Bridlington' },
-    { ground: '', name: 'Dronfield' },
-    { ground: '', name: 'Malton & Norton' },
-    { ground: '', name: 'Moortown' },
-    { ground: '', name: 'Old Brodleians' },
-    { ground: '', name: 'Pontefract' },
-    { ground: '', name: 'Doncaster Phoenix' },
-    { ground: '', name: 'Keighley' },
-    { ground: '', name: 'Wetherby' },
-    { ground: '', name: 'Huddersfield' },
-    { ground: '', name: 'Cleckheaton' },
-];
+export enum Team {
+  MAROONS = 'Maroons',
+  CAVALIERS = 'Cavaliers',
+}
 
-export const findTeam = (team: string): Club => {
-    throw new Error('cound not find the right club');
-};
+const opposition: Club[] = [
+    { ground: 'The Jim Saynor Ground, Station View, Harrogate, HG2 7JA', name: 'Harrogate Pythons' },
+    { ground: 'none', name: 'Fire Service' },
+    { ground: 'The Clubhouse, North Dean, Stainland Road, Greetland, Halifax, HX4 8LS', name: 'Heath' },
+    { ground: 'Standeven House, Broomfield Avenue, Halifax, West Yorkshire, HX3 0JE', name: 'Old Crossleyans' },
+    { ground: 'Westfield Banks, Westfield Lane, Goole, East Yorkshire', name: 'Goole' },
+    { ground: 'Rochdale RUFC Clubhouse, Moorgate Avenue, off Bury Road, Rochdale, Lancashire, OL11 5LU', name: 'Rochdale' },
+    { ground: 'Silver Royd, 569 Scalby Road, Scalby, Scarborough, YO13 0NL', name: 'Scarborough' },
+    { ground: 'Sandhill Lane, Selby, North Yorkshire, YO8 4JP', name: 'Selby' },
+    { ground: 'Glossop RUFC, Hargate Hill Lane, Charlesworth, Glossop, Derbyshire, SK13 5HG', name: 'Glossop' },
+    { ground: 'Dukes Park Queensgate, Bridlington, YO16 7LN', name: 'Bridlington' },
+    { ground: 'Gosforth Fields, Bubnell Road, Dronfield Woodhouse, Dronfield, S18 8QY', name: 'Dronfield' },
+    { ground: 'The Gannock, Old Malton Road, Malton, United Kingdom, North Yorkshire, YO17 7EY', name: 'Malton & Norton' },
+    { ground: 'Moss Valley, Leeds, LS17 7NT', name: 'Moortown' },
+    { ground: 'Woodhead, Hipperholme, Halifax, West Yorkshire, HX3 8JU', name: 'Old Brodleians' },
+    { ground: 'Moor Lane, Carleton, Pontefract, West Yorkshire, WF8 3RX', name: 'Pontefract' },
+    { ground: 'Doncaster Rugby Club, Castle Park, Doncaster, South Yorkshire, DN2 5QB', name: 'Doncaster Phoenix' },
+    { ground: 'Skipton Road, Utley, Keighley, West Yorkshire, BD20 6DT', name: 'Keighley' },
+    { ground: 'Wetherby RUFC, Grange Park, Wetherby, West Yorkshire, LS22 5NB', name: 'Wetherby' },
+    { ground: 'Lockwood Park, Brewery Drive, Huddersfield, HD4 6EN', name: 'Huddersfield' },
+    { ground: 'The Pavilion, Moorend, Cleckheaton, West Yorkshire, BD19 3UD', name: 'Cleckheaton' },
+    { ground: 'Warminster Road, Sheffield, South Yorkshire, S8 8PQ', name: 'Sheffield Engineers' },
+    { ground: 'Standbridge Lane, Sandal Magna, Wakefield, WF2 7DY', name: 'Sandal' },
+    { ground: '124 Ashby Road, Scunthorpe, DN16 2AG', name: 'Scunthorpe' },
+    { ground: 'Grove Park, Lower Greenfoot, Settle, BD24 9RB', name: 'North Ribblesdale' },
+    { ground: '???', name: 'Hallam' },
+    { ground: '1 Shay Lane, Bradford, BD9 6SL', name: 'Bradford Salem' },
+  ];
+
+  export const findTeam = (team: string): Club => {
+    if (/Y Cup|YC \?/.exec(team)) {
+      return {
+        name: 'Yorkshire Cup Reserve Date',
+      };
+    } else if (team === 'BREAK' || team === 'Reserve') {
+      return {
+        name: 'League Reserve Date',
+      };
+    } else if (/RFU CUP 1|PJ Cup/.exec(team)) {
+      return {
+        name: 'National Cup Reserve Date',
+      };
+    }
+
+    if (!team) {
+      throw new Error('no opposition specified');
+    }
+    if (team === 'TBC') {
+      throw new Error('no fixture!');
+    }
+    const oppo = opposition.find((club: Club) => club.name === team.replace(/ 2\??/, ''));
+    if (!oppo) {
+      throw new Error(`could not find the right club "${team}"`);
+    }
+    return oppo;
+  };
 
 export const home = 'Morley Rugby Football Club, Scatcherd Lane, Morley, Leeds, LS27 0JJ';
